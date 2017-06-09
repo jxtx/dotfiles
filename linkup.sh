813c25743db725194ba864671c34a7998ea3c71a
James Taylor <james@jamestaylor.org>
2017-06-09 14:01:17 -0400

    More renaming

--- linkup.sh
+++ linkup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -fs $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -fs $HOME/dotfiles/gitconfig $HOME/.gitconfig
+ln -fs $HOME/dotfiles/bspwm $HOME/.config/bspwm
+ln -fs $HOME/dotfiles/sxhkd $HOME/.config/sxhkd
+ln -fs $HOME/dotfiles/polybar $HOME/.config/polybar
